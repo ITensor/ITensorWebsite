ac38ac89e5131b4828c116f24c1210092d5a53af
Miles <miles.stoudenmire@gmail.com>
2023-02-11 10:16:13 -0500

    New Journal command for papers

--- gen_docs.py
+++ gen_docs.py
@@ -122,11 +122,14 @@ def openMDFile(vdocpath,page):
     return mdfile
 
 def convert(string,vers,lang):
-    #Convert SciPost[Vol,Issue,PageNum]tags
+    #Convert SciPost[Vol,Issue,PageNum] tags
     string = re.sub(r"SciPost\[(\d+),(\d+),(\d+)\]",r"<i style='color:#CC0000'>SciPost Phys.</i>&nbsp;&nbsp;<b>\1</b> <a href='https://scipost.org/10.21468/SciPostPhys.\1.\2.\3'>\3</a>",string)
 
-    #Convert PhysRev[Letter,Vol,PageNum]tags
+    #Convert PhysRev[Letter,Vol,PageNum] tags
     string = re.sub(r"PhysRev\[(.+?),(\d+),(\d+)\]",r"<i style='color:#CC0000'>Phys.&nbsp;Rev.&nbsp;\1</i>&nbsp;&nbsp;<b>\2</b> <a href='https://doi.org/10.1103/PhysRev\1.\2.\3'>\3</a>",string)
+
+    #Convert Journal[Name,Vol,PageNum,Url] tags
+    string = re.sub(r"Journal\[(.+?),(\d+),(\d+),(.+?)\]",r"<i style='color:#CC0000'>\1</i>&nbsp;&nbsp;<b>\2</b> <a href='\4'>\3</a>",string)
 
     #Convert arxiv:####.#### links
     string = re.sub(r"arxiv:(\d\d\d\d\.\d+)",r"arxiv:<a target='_blank' href='http://arxiv.org/abs/\1'>\1</a>",string)
